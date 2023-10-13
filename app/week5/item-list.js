b76b9267ddb97e56e04ee50bd074b3062a9129e0
Sergio Martinez <maikiby123@gmail.com>
2023-10-13 13:42:41 -0600

    fixed sort functions

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -1,49 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Item from './item';
 import items from './items.json';
 
 export default function ItemList() {
     
     const [sortBy, setSortBy] = useState('name');
-    
-    useEffect(() => {
-      
-        const initialSort = () => {
-            switch (sortBy) {
-                case 'name':
-                    items.sort((a, b) => a.name.localeCompare(b.name));
-                    break;
-                case 'category':
-                    handleClickSortByCategory();
-                    break;
-            }
-        }
-        
-        initialSort();
-
-    }, [items, sortBy]);
 
     function handleClickSortByName(e) {
-        
-        console.log('Clicked name!');
-        
-        items.sort((a, b) => a.name.localeCompare(b.name));
-
         setSortBy('name');
     }
 
     function handleClickSortByCategory(e) {
-        
-        console.log('Clicked category!');
-        
-        items.sort((a, b) => a.category.localeCompare(b.category));
-
         setSortBy('category');
     }
 
     function handleClick(e) {
-        
-        console.log('Clicked!');
 
         switch (sortBy) {
             
@@ -58,13 +29,18 @@ export default function ItemList() {
 
     }
 
+    if (sortBy === 'name') {
+        items.sort((a, b) => a.name.localeCompare(b.name));
+    }
+    else if (sortBy === 'category') {
+        items.sort((a, b) => a.category.localeCompare(b.category));
+    }
+
     return (
         <div>
-            
-            //TODO: Make the buttons actually work, stylize the page
             <div className='flex items-center'>
                 <button onClick={handleClickSortByName} className='m-2 p-2 border border-white rounded hover:bg-white hover:text-black btn'>Sort by Name</button>
-                <button onClick={handleClickSortByCategory} className='m-2 p-2 border border-white rounded hover:bg-white hover:text-black btn'>Sort by Category</button>
+                <button onClick={handleClickSortByCategory} className='m-2 p-2 border border-white rounded hover:bg-white hover:text-black btn'>Sort by Category</button> 
             </div>
 
             <div className='flex flex-col flex-grow items-center'>
