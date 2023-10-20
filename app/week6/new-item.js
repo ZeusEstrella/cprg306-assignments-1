f1c575d313b072b644b140874395206a944a334f
Sergio Martinez <maikiby123@gmail.com>
2023-10-20 13:54:59 -0600

    Week 6 Assignment

---
+++ app/week6/new-item.js
@@ -0,0 +1,75 @@
+"use client";
+import UseState, { useState } from "react";
+
+export default function NewItem({onAddItem}) {
+    
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState("produce");
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleQuantityChange = (event) => {
+        
+        const parseInput = parseInt(event.target.value);
+
+        if (!isNaN(parseInput)) {
+            setQuantity(parseInput);
+        }
+    }
+
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value);
+    }
+
+    const handleSubmit = (event) => { 
+        event.preventDefault();
+
+        const id = Math.random();
+        const item = {id, name, quantity, category};
+        console.log("Item Created: ", item);
+        
+        onAddItem(item);
+
+        setName("");
+        setQuantity(1);
+        setCategory("produce");
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                
+                <label htmlFor="name">Name</label>
+                <input required type="text" value={name} onChange={handleNameChange} className="text-white"/>
+                <pre>{'\n'}</pre>
+
+                <label htmlFor="name">Quantity</label>
+                <input required type="number" min={1} max={99} value={quantity} onChange={handleQuantityChange} className="text-white"/>
+                <pre>{'\n'}</pre>
+
+                <label htmlFor="quantity">Category {'\n'}
+                    <select onChange={handleCategoryChange} className="bg-white text-black">
+                        <option value="produce">Produce</option>
+                        <option value="dairy">Dairy</option>
+                        <option value="bakery">Bakery</option>
+                        <option value="meat">Meat</option>
+                        <option value="frozenFoods">Frozen Foods</option>
+                        <option value="cannedFoods">Canned Goods</option>
+                        <option value="dryGoods">Dry Goods</option>
+                        <option value="beverages">Beverages</option>
+                        <option value="snacks">Snacks</option>
+                        <option value="household">Household</option>
+                        <option value="other">Other</option>
+                    </select>
+                </label>
+                <pre>{'\n'}</pre>
+
+                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add Item</button>
+
+            </form>
+        </div>
+    )
+}
--- app/week6/page.js
+++ app/week6/page.js
@@ -1,12 +1,32 @@
-import Link from 'next/link';
+"use client";
 
+import Link from "next/link";
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import ItemData from "./items.json";
+import { useState } from "react";
 
 export default function Page() {
-    return(
+
+    const [items, setItems] = useState(ItemData);
+
+    function handleAddItem(item) {
+        setItems([...items, item]);
+    }
+
+    return (
         <main>
             <Link href='/' className="hover:underline ">
-                🏠Home
+                    🏠Home
             </Link>
+
+            <h1 className="m-2 p-2 font-bold text-xl">Shopping List</h1>
+            <ItemList
+                items={items}
+            />
+            <NewItem onAddItem={handleAddItem}/>
+            
         </main>
     );
+
 }
