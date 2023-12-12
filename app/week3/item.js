0fe1e87407dc905dceeebbb1a15af01839cbd8d4
ZeusEstrella <115190529+ZeusEstrella@users.noreply.github.com>
2023-12-11 18:52:19 -0700

    tweak 2

--- app/week3/item.js
+++
@@ -1,9 +0,0 @@
-export default function Item({name, quantity, category}) {
-    return (
-        <div className="border border-pink-600 m-5 space-y-1 p-2">
-            <h2>{name}</h2>
-            <p>Quantity: {quantity}</p>
-            <p>Category: {category}</p>
-        </div>
-    );
-}
